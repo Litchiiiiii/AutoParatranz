03b4d706b4c9cefc299eff40b5e8bd6a18c06cf8
Litchiiiiiii <139540811+Litchiiiiii@users.noreply.github.com>
2024-03-14 15:34:37 +0000

    1

--- translationToGitHub.py
+++ translationToGitHub.py
@@ -5,21 +5,26 @@ projectId = 9584
 fileId = 1282824
 
 fileUrl = "https://paratranz.cn/api/projects/" + str(projectId) + "/files/"
-fileList = []
+fileTranslationUrl = "https://paratranz.cn/api/projects/" + str(projectId) + "/files/" + str(id) + "/translation"
+fileIdList = []
+filePathList = []
 
 def translate(id):
-    resourcePackUrl = "https://paratranz.cn/api/projects/" + str(projectId) + "/files/" + str(id) + "/translation"
-    urlRequests = requests.get(resourcePackUrl, headers={"Authorization": token, "accept": "*/*"})
+    urlRequests = requests.get(fileTranslationUrl, headers={"Authorization": token, "accept": "*/*"})
     print(urlRequests.json())
 
 def getFile():
     fileRequest = requests.get(fileUrl, headers={"Authorization": token, "accept": "*/*"})
     fileJson = fileRequest.json()
     for i in fileJson:
-        fileList.append(i["id"])
+        fileIdList.append(i["id"])
+    for n in fileJson:
+        filePathList.append(n["name"])
 
 
 if __name__ == '__main__':
     getFile()
-    for v in fileList:
-        translate(v)
+    #for v in fileList:
+        #translate()
+    for v in filePathList:
+        print(v)
