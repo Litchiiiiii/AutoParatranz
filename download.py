eed9f4f0c14769f4a868a713fb5a6792d70c2473
Litchiiiiiii <139540811+Litchiiiiii@users.noreply.github.com>
2024-03-14 14:16:26 +0000

    1

--- download.py
+++ download.py
@@ -6,11 +6,11 @@ from urllib3 import response
 token = os.environ["API_KEY"]
 projectId = 9584
 fileId = 1282824
-resourcePackUrl = "https://paratranz.cn/api/projects/" + projectId + "/files/" + fileId
+resourcePackUrl = "https://paratranz.cn/api/projects/" + str(projectId) + "/files/" + str(fileId)
 
 
 async def translate():
     urlRequests = requests.get(resourcePackUrl, headers={"Authorization": token, "accept": "*/*"})
     print(urlRequests.content)
 if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(translate())
+    asyncio.run(translate())
