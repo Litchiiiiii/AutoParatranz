30c21d29cc5d698f0dec9df84453681ea831a127
Litchiiiiiii <139540811+Litchiiiiii@users.noreply.github.com>
2024-03-14 14:20:58 +0000

    1

--- download.py
+++ download.py
@@ -7,10 +7,18 @@ token = os.environ["API_KEY"]
 projectId = 9584
 fileId = 1282824
 resourcePackUrl = "https://paratranz.cn/api/projects/" + str(projectId) + "/files/" + str(fileId)
+fileUrl = "https://paratranz.cn/api/projects/" + str(projectId) + "/files/"
 
 
 async def translate():
     urlRequests = requests.get(resourcePackUrl, headers={"Authorization": token, "accept": "*/*"})
-    print(urlRequests.content)
+    print(urlRequests.content["name"])
+
+
+def getFile():
+    fileRequest = requests.get(fileUrl, headers={"Authorization": token, "accept": "*/*"})
+    print(fileRequest.content["id"])
+
+
 if __name__ == '__main__':
     asyncio.run(translate())
