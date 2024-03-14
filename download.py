0821a375a7fe8563bad9e5843843dd0cdf350858
Litchiiiiiii <139540811+Litchiiiiii@users.noreply.github.com>
2024-03-14 13:41:39 +0000

    1

--- download.py
+++ download.py
@@ -1,28 +1,27 @@
 import asyncio
 import os
+
 import paratranz_client
 
-# Defining the host is optional and defaults to https://paratranz.cn/api
-# See configuration.py for a list of all supported configuration parameters.
 configuration = paratranz_client.Configuration(
     host="https://paratranz.cn/api"
 )
 configuration.api_key['Token'] = os.environ["API_KEY"]
 
 
-async def download():
+async def translate():
     async with paratranz_client.ApiClient(configuration) as api_client:
-        # Create an instance of the API class
-        api_instance = paratranz_client.ArtifactsApi(api_client)
+        api_instance = paratranz_client.FilesApi(api_client)
         project_id = 9584  # int | 项目ID
+        file_id = 1282824  # int | 文件ID
 
         try:
-            # 下载
-            api_response = await api_instance.download_artifact(project_id)
+            # 文件翻译
+            api_response = await api_instance.get_file_translation(project_id, file_id)
             print(api_response)
         except Exception as e:
-            print("Exception when calling ArtifactsApi->download_artifact: %s\n" % e)
+            print("Exception when calling FilesApi->get_file_translation: %s\n" % e)
 
 
 if __name__ == '__main__':
-    asyncio.run(download())
+    asyncio.run(translate())
