e61af11e04deb743e01f6ad16885244d26148fa2
Litchiiiiiii <139540811+Litchiiiiii@users.noreply.github.com>
2024-03-14 14:00:54 +0000

    1

--- download.py
+++ download.py
@@ -19,8 +19,8 @@ async def translate():
 
         try:
             # 文件翻译
-            api_response = await api_instance.get_file_translation(project_id, file_id)
             create_file200_response_instance = CreateFile200Response.from_json(json)
+            api_response = await api_instance.get_file_translation(project_id, file_id)
             print(create_file200_response_instance)
             print(api_response)
         except Exception as e:
