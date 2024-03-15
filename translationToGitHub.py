71adbc5fb269bd5194e3d77523e7e90f62dab8dc
Litchiiiiiii <139540811+Litchiiiiii@users.noreply.github.com>
2024-03-15 11:46:23 +0000

    1

--- translationToGitHub.py
+++ translationToGitHub.py
@@ -40,6 +40,7 @@ def listClear():
     value.clear()
     key.clear()
     zh_cn.clear()
+
 def linkgithub():
     auth = Auth.Token(gittoken)
     g = Github(auth = auth)
@@ -48,7 +49,7 @@ def linkgithub():
 
 if __name__ == '__main__':
     getFile()
-    '''
+    
     for id in fileIdList:
         i = 0
         listClear()
@@ -68,5 +69,5 @@ if __name__ == '__main__':
                 f.seek(0)    #读出前将指针移到文件开头
                 cNames = f.read()    #文件所有行读出，此处也可以使用read（）函数，结果一样 
         #print(zh_cn)
-    '''
-    linkgithub()
+    
+    #linkgithub()
