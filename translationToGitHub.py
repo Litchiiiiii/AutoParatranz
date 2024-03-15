56d79c43d4c0a555c809b3045ec111e917a6585c
Litchiiiiiii <139540811+Litchiiiiii@users.noreply.github.com>
2024-03-15 13:24:05 +0000

    1

--- translationToGitHub.py
+++ translationToGitHub.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
                 os.makedirs("Patch-Pack-CN/" + path,0o777,True)
             with open("Patch-Pack-CN/" + path + filename, "w+", encoding='UTF-8') as f:    #读操作与写操作
                 f.write(str(zh_cn))    #写入
-                f.seek(0)    #读出前将指针移到文件开头
+                f.close()
                 #cNames = f.read()    #文件所有行读出，此处也可以使用read（）函数，结果一样 
             print("上传完成：" + path + filename)
     
