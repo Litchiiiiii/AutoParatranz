93ec567c422f8562fb50cae7056cd0599f58a785
Litchiiiiiii <139540811+Litchiiiiii@users.noreply.github.com>
2024-06-21 22:57:57 +0800

    1

--- translationToGitHub.py
+++ translationToGitHub.py
@@ -1,10 +1,10 @@
 import json
 import os
 import re
+from typing import List, Dict, Tuple
 
 import requests
-from github import Github
-from github import Auth
+from github import Github, Auth
 
 # 从环境变量中获取必要的Token和项目ID
 token = os.environ["API_TOKEN"]
@@ -13,12 +13,17 @@ project_id = os.environ["PROJECT_ID"]
 file_url = f"https://paratranz.cn/api/projects/{project_id}/files/"
 
 # 初始化列表和字典
-file_id_list, file_path_list, zh_cn_list = [], [], []
+file_id_list: List[int] = []
+file_path_list: List[str] = []
+zh_cn_list: List[Dict[str, str]] = []
 
 
-def translate(file_id):
+def translate(file_id: int) -> Tuple[List[str], List[str]]:
     """
-    获取指定文件的翻译内容，并返回键值对列表。
+    获取指定文件的翻译内容并返回键值对列表。
+
+    :param file_id: 文件ID
+    :return: 包含键和值的元组列表
     """
     url = f"https://paratranz.cn/api/projects/{project_id}/files/{file_id}/translation"
     response = requests.get(url, headers={"Authorization": token, "accept": "*/*"})
@@ -29,14 +34,14 @@ def translate(file_id):
         keys.append(item["key"])
         translation = item["translation"]
         original = item["original"]
-        values.append(original if not(translation) and (item["stage"]==0 or item["stage"] == -1) else translation)
+        values.append(original if not translation and (item["stage"] == 0 or item["stage"] == -1) else translation)
 
     return keys, values
 
 
-def get_files():
+def get_files() -> None:
     """
-    获取项目中的文件列表，并提取文件ID和路径。
+    获取项目中的文件列表并提取文件ID和路径。
     """
     response = requests.get(file_url, headers={"Authorization": token, "accept": "*/*"})
     files = response.json()
@@ -46,9 +51,12 @@ def get_files():
         file_path_list.append(file["name"])
 
 
-def save_translation(zh_cn_dict, path):
+def save_translation(zh_cn_dict: Dict[str, str], path: str) -> None:
     """
     保存翻译字典为JSON文件。
+
+    :param zh_cn_dict: 翻译字典
+    :param path: 文件路径
     """
     dir_path = os.path.join("Patch-Pack-CN", os.path.dirname(path))
     os.makedirs(dir_path, exist_ok=True)
@@ -58,7 +66,7 @@ def save_translation(zh_cn_dict, path):
         json.dump(zh_cn_dict, f, ensure_ascii=False, indent=4, separators=(',', ':'))
 
 
-def main():
+def main() -> None:
     get_files()
 
     for file_id, path in zip(file_id_list, file_path_list):
@@ -66,7 +74,7 @@ def main():
         zh_cn_dict = {key: re.sub(r'\\n', '\n', value) for key, value in zip(keys, values)}
         zh_cn_list.append(zh_cn_dict)
         save_translation(zh_cn_dict, path)
-        print(f"上传完成：{path}")
+        print(f"上传完成：{re.sub('en_us.json','zh_cn.json',path)}")
 
 
 if __name__ == '__main__':
